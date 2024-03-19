a926c1f1af26130b8a0cc338f4b702c32e700121
ayanmirza99 <125931448+ayanmirza99@users.noreply.github.com>
2024-03-20 03:48:20 +0500

    client set up

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Ayan Mirza</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
